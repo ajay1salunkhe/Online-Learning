72f6037c62aa10f60d4d3d8c8b560723a635ab3b
prachi <prachikulkarni2112@gmail.com>
2018-10-16 16:00:28 +0530

    registration connection

--- linkss/insert.php
+++ linkss/insert.php
@@ -1,13 +1,6 @@
 <?php
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "", "online_learning");
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
- 
+include("connection.php");
+
 // Escape user inputs for security
 $username = mysqli_real_escape_string($link, $_REQUEST['inputUsername1']);
 $email = mysqli_real_escape_string($link, $_REQUEST['inputEmail1']);
@@ -16,9 +9,10 @@ $password = mysqli_real_escape_string($link, $_REQUEST['inputPassword1']);
 // attempt insert query execution
 $sql = "INSERT INTO signup (username, email, password) VALUES ('$username', '$email', '$password')";
 if(mysqli_query($link, $sql)){
-    echo "Sign Up successfully.";
+    header("Location: login_redirect.html");
 } else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+	/*echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);*/
+	echo "Error while Registering.";
 }
  
 // close connection
